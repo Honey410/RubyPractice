bd5f11f9dfe6c4e546ec2e1df10d60cd43ec930f
Honey410 <honayjhamnani410@gmail.com>
2021-07-06 14:52:42 +0530

    Exercise

---
+++ Ex.rb
@@ -0,0 +1,45 @@
+#puts "Hello World!"
+#puts "Name is Honey"
+
+#Comments
+# puts "This will ignored."
+#puts "This will run."
+
+#NUMBERS
+#puts "What is 3 + 2? #{3 + 2}"#Valid Expression
+#puts "Is it true that 3 + 2 < 5 - 7?" #Invalid_Epressions
+#puts "Is greater? #{6 > -2}"
+#puts "Is greater or equal? #{-2 >= -2}"
+#puts "Is less or equal? #{1 <= -2}"
+
+
+#VARIABLES
+#cars = 100 #variable
+#puts "There are #{cars} cars available.
+
+#my_age = 90
+#my_height = 74 # inches
+#my_weight = 180
+#adding variable values 
+#puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+
+#STRING
+#binary = "binary" #grp of char
+#a = "Those who know #{binary}"
+#puts a
+#w = "ABCDE"
+#e = "FGHIJ."
+#puts w + e  #CONCATINATION
+#puts "." * 10  #print dot 10 times
+#puts "*" * 10  #print * 10 times
+
+#end1 = "C"
+#end2 = "h"
+#end3 = "e"
+#end4 = "e"
+#end5 = "s"
+#end6 = "e"
+#puts end1 + end2 + end3 + end4 + end5 + end6
+#puts end1 + end2 + end3 + end4 + end5 + end6
+#PUTS prints in new line
+
